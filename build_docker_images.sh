edd5ef6642157c5d1021a2fc1c7827014e598701
Matthew Elwin <elwin@northwestern.edu>
2024-01-15 14:42:53 -0600

    Build docker images script

---
+++ build_docker_images.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+script_name=$0
+usage=\
+"Usage: $script_name [<target>]
+
+Build the rosberrypi docker images
+
+<target> - Build only the images up-to the named target
+           allowing a more minimal image to be explicitly referred to and booted
+"""
+set -e
+
+if [ $# -gt "1" ]; then
+    printf "$usage"
+    exit 1
+elif [ $# = "0" ]; then
+    target=rosberrypi-ros
+else
+    target=$1
+fi
+
+# determine if we need to use sudo when calling docker
+if groups $USER | grep -q docker;
+then
+    DOCKER=docker
+else
+    DOCKER="sudo docker"
+fi
+
+$DOCKER build --target $target --tag $target ./rosberrypi-ros
+$DOCKER build --tag rosberry-writer ./rosberry-writer
